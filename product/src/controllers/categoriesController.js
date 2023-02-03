ae198283027119bd5402b6eca07ecd4809539a7c
Trindade Ash Fonseca <n920601@gruposantander.com>
2023-02-03 12:05:33 -0300

    Detalhamento de uma Categoria

--- product/src/controllers/categoriesController.js
+++ product/src/controllers/categoriesController.js
@@ -8,6 +8,17 @@ class CategoryController {
         })
     }
 
+    static getCategoryById = (req, res) => {
+        const { id } = req.params
+        categories.findById(id, (err, category) => {
+            if (err) {
+                res.status(500).send({message: err.message})
+            } else {
+                res.status(201).json(category)
+            }
+        })
+    }
+
     static createNewCategory = (req, res) => {
         const { error } = validateCategory(req.body)
         if (error) throw error
--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 router
     .get('/api/categories', CategoriesController.listAllCategories)
-    .post('/api/admin/categories', CategoriesController.createNewCategory)
+    .get('/api/categories/:id', CategoriesController.getCategoryById)
+    .post('/api/admin/categories', CategoriesController.createNewCategory)    
 
 module.exports = router
