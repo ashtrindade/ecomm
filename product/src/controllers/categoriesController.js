fc8c62b89265da0dc3cc7a8dd28a967cbe5e6efb
Trindade Ash Fonseca <n920601@gruposantander.com>
2023-02-03 11:56:10 -0300

    Inserção de Categorias

--- product/src/controllers/categoriesController.js
+++ product/src/controllers/categoriesController.js
@@ -1,10 +1,25 @@
 const categories = require('../models/category')
-
+const validateCategory = require('../validations/validateCategory')
 class CategoryController {
     static listAllCategories = (req, res) => {
         categories.find((err, Categories) => {
             console.log(err)
             res.status(200).json(Categories)
+        })
+    }
+
+    static createNewCategory = (req, res) => {
+        const { error } = validateCategory(req.body)
+        if (error) throw error
+
+        const category = new categories(req.body)
+
+        category.save((err) => {
+            if (err) {
+                res.status(500).send({message: `${err.message} - Failed`})
+            } else {
+                res.status(201).send(category.toJSON())
+            }
         })
     }
 }
--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router
     .get('/api/categories', CategoriesController.listAllCategories)
+    .post('/api/admin/categories', CategoriesController.createNewCategory)
 
 module.exports = router
