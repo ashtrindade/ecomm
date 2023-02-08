1a29b764d8900e4b1a9c230df1d421ffbe60b5bc
Trindade Ash Fonseca <n920601@gruposantander.com>
2023-02-08 16:39:14 -0300

    Configs gerais

--- finance/src/index.js
+++ finance/src/index.js
@@ -1,14 +1,11 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const PORT = process.env.PORT || 3003
+const routes = require('./api/routes/index')
 
+const PORT = process.env.PORT || 3003
 const app = express()
 
-app.use(bodyParser.json())
+routes(app)
 
-app.get('/', (req, res) =>
-    res.status(200)
-        .send({ "API": "Homepage" }))
 app.listen(PORT, () => console.log(`🟢 - Server running on PORT ${PORT}`))
 
 module.exports = app
