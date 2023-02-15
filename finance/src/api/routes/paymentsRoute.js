1e8cf07e47153af56cbc20ee59bd5de44e82cdf8
Trindade Ash Fonseca <n920601@gruposantander.com>
2023-02-15 08:41:20 -0300

    fix: remove admin from routes

--- finance/src/api/routes/paymentsRoute.js
+++ finance/src/api/routes/paymentsRoute.js
@@ -4,8 +4,8 @@ const PaymentController = require('../controllers/paymentController')
 const router = express.Router()
 
 router
-    .get('/api/admin/payments', PaymentController.getAllPayments)
-    .get('/api/admin/payments/:id', PaymentController.getPaymentById)
-    .post('/api/admin/payments/', PaymentController.addNewPayment)
-    .patch('/api/admin/payments/:id', PaymentController.updateStatus)
+    .get('/api/payments', PaymentController.getAllPayments)
+    .get('/api/payments/:id', PaymentController.getPaymentById)
+    .post('/api/payments/', PaymentController.addNewPayment)
+    .patch('/api/payments/:id', PaymentController.updateStatus)
 module.exports = router
