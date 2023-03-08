d136811b5bf932f5110653e68bc0c563e1c1199c
Ash Trindade <ash.trindade@pagonxt.com>
2023-03-08 08:18:09 -0300

    fix: update validations

--- account/src/validations/validateAccount.js
+++ account/src/validations/validateAccount.js
@@ -3,15 +3,15 @@ const joi = require('joi')
 const validateAccount = (account) => {
     const schema = joi.object().keys({
         name: joi.string()
-            .min(3)
-            .pattern(new RegExp(/^\D/))
-            .required(),
-        email: joi.string(),
-        password: joi.string(),
+            .pattern(new RegExp(/^\D/)),
+        email: joi.string()
+            .pattern(new RegExp(/\S+@\S+\.\S+/i)),
+        password: joi.string()
+            .pattern(new RegExp(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,32}$/)),
         cpf: joi.string()
             .pattern(new RegExp(/^\d{11}$/)),
         phone: joi.string()
-            .pattern(new RegExp(/^\d{10,13}$/)),
+            .pattern(new RegExp(/^\d{10,13}$/))
     })
 
     const result = schema.validate(account)
