98e32b433eae83c130a592af0dab2a5057c5ed10
Ash Trindade <ash.trindade@pagonxt.com>
2023-03-10 10:00:44 -0300

    feat: set authentication to routes

--- account/src/routes/accountRoutes.js
+++ account/src/routes/accountRoutes.js
@@ -1,11 +1,36 @@
 const express = require('express')
+const passport = require('passport')
 const AccountsController = require('../controllers/accountsController')
 
 const router = express.Router()
 
 router
-    .get('/api/accounts', AccountsController.listAllAccounts)
-    .get('/api/admin/accounts/:id', AccountsController.getAccountById)
-    .post('/api/admin/accounts', AccountsController.createNewAccount)    
+    .get(
+        '/api/accounts',
+        passport.authenticate('jwt', { session: false }),
+        AccountsController.listAllAccounts
+    )
+    .get(
+        '/api/accounts/:id',
+        AccountsController.getAccountById
+    )
+    .post(
+        '/api/accounts',
+        AccountsController.createNewAccount
+    )
+    .post(
+        '/api/accounts/login',
+        AccountsController.userLogin
+    )
+    .put(
+        '/api/accounts/:id',
+        passport.authenticate('jwt', { session: false }),
+        AccountsController.updateAccount
+    )
+    .delete(
+        '/api/accounts/:id',
+        passport.authenticate('jwt', { session: false }),
+        AccountsController.deleteAccount
+    )
 
 module.exports = router
