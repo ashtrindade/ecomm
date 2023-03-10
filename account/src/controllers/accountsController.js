3cb6fed7986df51574a3926257f8a622096f77be
Ash Trindade <ash.trindade@pagonxt.com>
2023-03-10 10:00:00 -0300

    feat: update CRUD & add login

--- account/src/controllers/accountsController.js
+++ account/src/controllers/accountsController.js
@@ -1,12 +1,17 @@
 const accounts = require('../models/account')
-const encryptPassword = require('../helpers/passwordEncrypt')
+const encryptPassword = require('../helpers/encryptPassword')
+const validatePassword = require('../helpers/validatePassword')
+const issueJWT = require('../helpers/issueJwt')
 const validateAccount = require('../validations/validateAccount')
 class AccountsController {
 
     static listAllAccounts = (req, res) => {
         accounts.find((err, accounts) => {
-            console.log(err)
-            res.status(200).json(accounts)
+            if (err) {
+                return res.status(500).send({ msg: err.message })
+            } else {
+                return res.status(200).json(accounts)
+            }
         })
     }
 
@@ -14,9 +19,9 @@ class AccountsController {
         const { id } = req.params
         accounts.findById(id, (err, account) => {
             if (err) {
-                res.status(500).send({ message: err.message })
+                return res.status(500).send({ message: err.message })
             } else {
-                res.status(200).json(account)
+                return res.status(200).json(account)
             }
         })
     }
@@ -31,10 +36,57 @@ class AccountsController {
         const account = new accounts(req.body)
         account.save((err) => {
             if (err) {
-                res.status(500).send({ message: `${err.message} - Failed` })
+                return res.status(500).send({ message: `${err.message} - Failed` })
             } else {
-                res.status(201).send(account.toJSON())
+                return res.status(201).send(account.toJSON())
             }
+        })
+    }
+
+    static userLogin = (req, res) => {
+        accounts.findOne({ email: req.body.email })
+            .then((user) => {
+                if (!user) {
+                    return res.status(400).json({ success: false, msg: 'Incorrect email or password ! Try again.' })
+                }
+
+                const isValid = validatePassword(req.body.password, user.password)
+
+                if (isValid) {
+                    const tokenObject = issueJWT(user)
+
+                    res.status(200).json({
+                        success: true,
+                        token: tokenObject.token,
+                        expiresIn: tokenObject.expires
+                    })
+                } else {
+                    return res.status(400).json({ success: false, msg: 'Incorrect email or password ! Try again.' })
+                }
+            })
+            .catch((err) => {
+                throw(err)
+            })
+    }
+
+    static updateAccount = (req, res) => {
+        const { id } = req.params
+        accounts.findByIdAndUpdate(id, { $set: req.body }, { new: true }, (err, account) => {
+            if (err) {
+                return res.status(500).send({ message: err.message })
+            }
+            return res.status(204).set('Location', `/admin/Accounts/${account.id}`).send({ message: 'Account successfully updated' })
+        })
+    }
+
+    static deleteAccount = (req, res) => {
+        const { id } = req.params
+
+        accounts.findByIdAndDelete(id, (err) => {
+            if (err) {
+                return res.status(500).send({ message: err.message })
+            }
+            return res.status(204).send({ message: 'Account successfully deleted' })
         })
     }
 }
